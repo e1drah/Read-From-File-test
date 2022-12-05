592c6d952addf0dd8a3a1bd4fe8b7303c305c3ae
e1drah <113189459+e1drah@users.noreply.github.com>
2022-12-05 12:39:33 -0400

    Initial commit

---
+++ Read From File test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read_From_File_test
+{
+    public class FileNotFoundException : System.IO.IOException { }
+    public static class File { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String text;
+            try
+            {
+                text = System.IO.File.ReadAllText("Test.txt");
+                Console.WriteLine(text);
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
